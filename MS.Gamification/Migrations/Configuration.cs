bd9c9798b530efed723f8262723800a1f9bdd620
Fern Hughes <fernjampel@hotmail.co.uk>
2016-04-06 00:48:16 +0100

    Finished creating data models for the Beta Campaign and seeded the database

--- MS.Gamification/Migrations/Configuration.cs
+++ MS.Gamification/Migrations/Configuration.cs
@@ -3,11 +3,13 @@
 // File: Configuration.cs  Created: 2016-03-23@23:23
 // Last modified: 2016-03-24@00:37 by Fern
 
+using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using MS.Gamification.Controllers;
+using MS.Gamification.DataAccess.EntityFramework6;
 using MS.Gamification.Models;
 
 namespace MS.Gamification.Migrations
@@ -41,6 +43,102 @@ namespace MS.Gamification.Migrations
             CreateAdministratorAccount(context);
             CreateCategories(context);
             context.SaveChanges();
+            CreateBetaMission(context);
+            context.SaveChanges();
+            }
+
+        void CreateBetaMission(ApplicationDbContext context)
+            {
+            var phaseCategory = context.Categories.Single(p => p.Name == "Phase");
+            var planetCategory = context.Categories.Single(p => p.Name == "Planet");
+            var openClusterCategory = context.Categories.Single(p => p.Name == "Open Cluster");
+            var galaxyCategory = context.Categories.Single(p => p.Name == "Galaxy");
+            context.Missions.AddOrUpdate(p => p.Name,
+                new Mission
+                    {
+                    Name = "Beta Mission",
+                    AwardTitle = "Beta Tester",
+                    Level = 1,
+                    Tracks = new List<MissionTrack>
+                        {
+                        new MissionTrack
+                            {
+                            Name = "Lunar Track",
+                            AwardTitle = "Beta Lunar Observer",
+                            Number=1,
+                            Challenges= new List<Challenge>
+                                {
+                                new Challenge
+                                    {
+                                    Name = "See the waxing crescent moon",
+                                    Points = 1,
+                                    BookSection = "Moon",
+                                    CategoryId = phaseCategory.Id,
+                                    Location = "Moon"
+                                    },
+                                new Challenge
+                                    {
+                                    Name = "See the full moon",
+                                    Points = 1,
+                                    BookSection = "Moon",
+                                    CategoryId = phaseCategory.Id,
+                                    Location = "Moon"
+                                    }
+                                }
+                            },
+                        new MissionTrack
+                            {
+                            Name = "Planetary Track",
+                            AwardTitle = "Beta Planetologist",
+                            Number=2,
+                            Challenges= new List<Challenge>
+                                {
+                                new Challenge
+                                    {
+                                    Name = "See Jupiter",
+                                    Points = 1,
+                                    BookSection = "Solar System",
+                                    CategoryId = planetCategory.Id,
+                                    Location = "Solar System"
+                                    },
+                                new Challenge
+                                    {
+                                    Name = "See Saturn",
+                                    Points = 1,
+                                    BookSection = "Solar System",
+                                    CategoryId = planetCategory.Id,
+                                    Location = "Solar System"
+                                    }
+                                }
+                            },
+                        new MissionTrack
+                            {
+                            Name = "Deep Space Track",
+                            AwardTitle = "Beta Deep Space Explorer",
+                            Number=3,
+                            Challenges= new List<Challenge>
+                                {
+                                new Challenge
+                                    {
+                                    Name = "See M45, The Pleiades",
+                                    Points = 1,
+                                    BookSection = "Winter",
+                                    CategoryId = openClusterCategory.Id,
+                                    Location = "Taurus"
+                                    },
+                                new Challenge
+                                    {
+                                    Name = "See M31, The Andromeda Galaxy",
+                                    Points = 1,
+                                    BookSection = "Autumn",
+                                    CategoryId = galaxyCategory.Id,
+                                    Location = "Andromeda"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                );
             }
 
         void CreateCategories(ApplicationDbContext context)
---
+++ MS.Gamification/Models/MissionTrack.cs
@@ -0,0 +1,24 @@
+﻿// This file is part of the MS.Gamification project
+// 
+// File: MissionTrack.cs  Created: 2016-04-05@23:10
+// Last modified: 2016-04-05@23:11 by Fern
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MS.Gamification.DataAccess;
+
+namespace MS.Gamification.Models
+    {
+    public class MissionTrack : IDomainEntity<int>
+        {
+        [Required]
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public virtual List<Challenge> Challenges { get; set; }
+        [Required]
+        public string AwardTitle { get; set; }
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        }
+    }
