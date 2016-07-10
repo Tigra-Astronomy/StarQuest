c319280c8258e15a538af9a7720cd80b10a86417
Fern Hughes <fernjampel@hotmail.co.uk>
2016-07-10 02:12:34 +0100

    Fixed GA-120 (button colour contrast) and also a problem with the display of enumerated types.

--- MS.Gamification/Models/TransparencyLevel.cs
+++ MS.Gamification/Models/TransparencyLevel.cs
@@ -1,3 +1,8 @@
+// This file is part of the MS.Gamification project
+// 
+// File: TransparencyLevel.cs  Created: 2016-05-10@22:29
+// Last modified: 2016-07-10@01:40
+
 using System.ComponentModel.DataAnnotations;
 
 namespace MS.Gamification.Models
@@ -5,16 +10,10 @@ namespace MS.Gamification.Models
     public enum TransparencyLevel
         {
         Unknown,
-
-        [Display(Name = "Extremely Clear")]
-        ExtremelyClear,
+        [Display(Name = "Extremely Clear")] ExtremelyClear,
         Clear,
-
-        [Display(Name = "Mostly Clear")]
-        MostlyClear,
-
-        [Display(Name = "Somewhat Clear")]
-        SomewhatClear,
+        [Display(Name = "Mostly Clear")] MostlyClear,
+        [Display(Name = "Somewhat Clear")] SomewhatClear,
         Poor
         }
     }
