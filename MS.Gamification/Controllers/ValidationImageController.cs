d7964f05faac4a0f807bed6353eb395931d38225
Fern Hughes <fernjampel@hotmail.co.uk>
2016-07-16 05:11:09 +0100

    Commit missing file for ValidationImageController

---
+++ MS.Gamification/Controllers/ValidationImageController.cs
@@ -0,0 +1,30 @@
+﻿// This file is part of the MS.Gamification project
+// 
+// File: ValidationImageController.cs  Created: 2016-07-10@00:07
+// Last modified: 2016-07-16@03:15
+
+using System.Web.Mvc;
+using MS.Gamification.HtmlHelpers;
+using Ninject;
+
+namespace MS.Gamification.Controllers
+    {
+    public class ValidationImageController : Controller
+        {
+        readonly IImageStore imageStore;
+
+        public ValidationImageController([Named("ValidationImageStore")] IImageStore imageStore)
+            {
+            this.imageStore = imageStore;
+            }
+
+        public ActionResult GetImage(string id)
+            {
+            if (id == null)
+                id = string.Empty;
+            var fullyQualifiedFileName = imageStore.FindImage(id);
+            var contentType = imageStore.MimeType(id);
+            return File(fullyQualifiedFileName, contentType);
+            }
+        }
+    }
