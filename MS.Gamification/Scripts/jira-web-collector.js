43239ce0564bc6b74b2911f942eefad8819ca932
Fern Hughes <fernjampel@hotmail.co.uk>
2016-07-28 21:32:45 +0100

    Updated the Jira web collector

--- MS.Gamification/Scripts/jira-web-collector.js
+++ MS.Gamification/Scripts/jira-web-collector.js
@@ -1,7 +1,7 @@
 ﻿// Requires jQuery!
 jQuery.ajax({
     url:
-        "https://tigra-astronomy.atlassian.net/s/3e08a9b87599596867d6f0450ac67431-T/-vrlul0/100005/b6b48b2829824b869586ac216d119363/1000.0.4/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-UK&collectorId=72ebdef2",
+        "https://tigra-astronomy.atlassian.net/s/e6419e2c5c0113ee3751d954220d9aa8-T/-6v7xy5/100008/3f41e4ee5204a9924f015364d1198c20/1000.0.4/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-UK&collectorId=72ebdef2",
     type: "get",
     cache: true,
     dataType: "script"
